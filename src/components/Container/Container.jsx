06805707217c18bc3b459d8a3defa0efd246e880
Numed <ruschinets09@gmail.com>
2022-10-31 01:01:34 +0200

    Create some functions

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -12,11 +12,190 @@ import {
   Square,
 } from "./style";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Container = () => {
   const [score, setScore] = useState(0);
   const [best, setBest] = useState(0);
+  const [matrix, setMatrix] = useState([
+    [" ", " ", " ", " "],
+    [" ", " ", " ", " "],
+    [" ", " ", " ", " "],
+    [" ", " ", " ", " "],
+  ]);
+  const [options, setOptions] = useState([
+    { x: 0, y: 0 },
+    { x: 0, y: 1 },
+    { x: 0, y: 2 },
+    { x: 0, y: 3 },
+    { x: 1, y: 0 },
+    { x: 1, y: 1 },
+    { x: 1, y: 2 },
+    { x: 1, y: 3 },
+    { x: 2, y: 0 },
+    { x: 2, y: 1 },
+    { x: 2, y: 2 },
+    { x: 2, y: 3 },
+    { x: 3, y: 0 },
+    { x: 3, y: 1 },
+    { x: 3, y: 2 },
+    { x: 3, y: 3 },
+  ]);
+
+  const blankedMatrix = () => {
+    let extra = [
+      [" ", " ", " ", " "],
+      [" ", " ", " ", " "],
+      [" ", " ", " ", " "],
+      [" ", " ", " ", " "],
+    ];
+    return extra;
+  };
+
+  useEffect(() => {
+    addNumber();
+    addNumber();
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    console.table(matrix);
+    // eslint-disable-next-line
+  }, [matrix]);
+
+  const clearBoard = () => {
+    setMatrix([
+      [" ", " ", " ", " "],
+      [" ", " ", " ", " "],
+      [" ", " ", " ", " "],
+      [" ", " ", " ", " "],
+    ]);
+  };
+
+  const addNumber = () => {
+    if (options.length > 0) {
+      let spotX = Math.floor(Math.random() * 4),
+        spotY = Math.floor(Math.random() * 4);
+
+      matrix[spotX][spotY] = Math.floor(Math.random() > 0.5) ? 2 : 4;
+      setMatrix([...matrix]);
+    }
+  };
+
+  const slide = (row) => {
+    let arr = row.filter((x) => x),
+      missing = 4 - arr.length,
+      zeros = Array(missing).fill(" ");
+
+    arr = zeros.concat(arr);
+    return arr;
+  };
+
+  const combine = (row) => {
+    for (let i = 3; i >= 1; i--) {
+      let a = row[i],
+        b = row[i - 1];
+
+      if (a === b) {
+        row[i] = a + b;
+        row[i - 1] = "";
+      }
+    }
+    return row;
+  };
+
+  const operate = (row) => {
+    row = slide(row);
+    row = combine(row);
+    row = slide(row);
+    return row;
+  };
+
+  const compare = (a, b) => {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (a[i][j] !== b[i][j]) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  const copyMatrix = (matrix) => {
+    let extra = [...matrix];
+    return extra;
+  };
+
+  const flip = (matrix) => {
+    console.log("MATRIX Before", matrix);
+    for (let i = 0; i < 4; i++) {
+      matrix[i].reverse();
+    }
+    console.log("MATRIX AFTER", matrix);
+    return matrix;
+  };
+
+  const rotate = (matrix) => {
+    let newMatrix = blankedMatrix();
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        newMatrix[i][j] = matrix[i][j];
+      }
+    }
+  };
+
+  window.addEventListener("keydown", (e) => {
+    let flipped = false;
+    let rotated = false;
+    if (e.key === "ArrowUp") {
+      flip(matrix);
+      setMatrix(matrix);
+      flipped = true;
+    } else if (e.key === "ArrowDown") {
+      console.log(e.key);
+    } else if (e.key === "ArrowLeft") {
+      flip(matrix);
+      matrix = rotate(matrix);
+      setMatrix(matrix);
+      rotated = true;
+    } else if (e.key === "ArrowRight") {
+      //right
+    } else {
+      return;
+    }
+    let pastMatrix = copyMatrix(matrix);
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i] = operate(matrix[i]);
+    }
+    let changed = compare(pastMatrix, matrix);
+
+    if (flipped) {
+      flip(matrix);
+    }
+
+    if (rotated) {
+      matrix = rotate(matrix);
+      matrix = rotate(matrix);
+      matrix = rotate(matrix);
+    }
+
+    if (changed) {
+      addNumber();
+    }
+  });
+
+  const CreateBoard = () => {
+    return matrix.map((el) => {
+      return el.map((element, index) => {
+        return (
+          <Square key={index} className="square no-value">
+            {element}
+          </Square>
+        );
+      });
+    });
+  };
 
   return (
     <SectionContainer>
@@ -35,26 +214,11 @@ const Container = () => {
               </Score>
             </ScoreContainer>
           </ScoreInner>
-          <RestartButton>New Game</RestartButton>
+          <RestartButton onClick={clearBoard}>New Game</RestartButton>
         </ScroreSection>
       </InfoContainer>
       <Board>
-        <Square className="no-value"></Square>
-        <Square className="white-blank">2</Square>
-        <Square className="sand">4</Square>
-        <Square className="orange-blank">8</Square>
-        <Square className="carrot-blank ">16</Square>
-        <Square className="carrot">32</Square>
-        <Square className="carrot-contast">64</Square>
-        <Square className="yellow-blank">128</Square>
-        <Square className="yellow">256</Square>
-        <Square className="yellow-contrast">512</Square>
-        <Square className="yellow-close-max ">1024</Square>
-        <Square className="yellow-max">2048</Square>
-        <Square className="no-value"></Square>
-        <Square className="no-value"></Square>
-        <Square className="no-value"></Square>
-        <Square className="no-value"></Square>
+        <CreateBoard />
       </Board>
     </SectionContainer>
   );
